df10d98fbc3ee28ca8ef5463581edf53ce559c94
elliottc32 <enc32690@ucmo.edu>
2025-02-13 16:14:00 -0600

    Continue work on Background Generation

---
+++ scenes/background.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var background_section: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _create_background(x_pos: int, y_pos: int) -> void:
+	pass
--- scenes/background_section.gd
+++ scenes/background_section.gd
@@ -1,5 +1,12 @@
 extends Sprite2D
 
+var x_step: int
+var y_step: int
+
+func _ready() -> void:
+	x_step = texture.get_width() / 2
+	y_step = texture.get_height() / 2
+
 signal player_entered
 
 func _on_area_2d_body_entered(body: Node2D) -> void:

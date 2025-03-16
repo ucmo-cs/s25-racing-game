d7bb5bd1cc5b68f7024196eb449ff3d34763efee
elliottc32 <enc32690@ucmo.edu>
2025-03-15 19:49:17 -0500

    Update Steering to finally work properly

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,56 +1,57 @@
 extends CharacterBody2D
 
-@export var max_speed: float
-@export var acceleration_speed: float
-@export var deceleration_speed: float
-@export var steer_speed: float
-@export var max_steer_angle: float
-@export var wheel_base: float
-
-var front_wheel: Node2D
-var back_wheel: Node2D
-var current_turn_angle: float
-
-func _ready() -> void:
-	front_wheel = %FrontWheelLocation
-	back_wheel = %BackWheelLocation
-	front_wheel.rotation_degrees = 0
-	back_wheel.rotation_degrees = 0
-	current_turn_angle = 0
-
+var wheel_base = 70  # Distance from front to rear wheel
+var steering_angle = 45  # Amount that front wheel turns, in degrees
+var steer_direction
+var engine_power = 400  # Forward acceleration force.
+var friction = -55
+var drag = -0.06
+var braking = -450
+var max_speed_reverse = 250
+var slip_speed = 400  # Speed where traction is reduced
+var traction_fast = 2.5 # High-speed traction
+var traction_slow = 10  # Low-speed traction
 
 func _physics_process(delta: float) -> void:
-	#Calculate Speed
-	var current_speed: float = velocity.length()
+	#Acceleration
+	var acceleration = Vector2.ZERO
+	
+	#Get Input
+	var turn = Input.get_axis("Left", "Right")
+	steer_direction = turn * deg_to_rad(steering_angle)
 	if Input.is_action_pressed("Gas"):
-		current_speed = min(current_speed + acceleration_speed, max_speed)
-	else:
-		current_speed = max(current_speed - deceleration_speed, 0)
-	velocity = velocity.normalized() * current_speed
-	#Calculate Steering
-	var turn_angle: float
-	if Input.is_action_pressed("Left"):
-		turn_angle = deg_to_rad(max(-1 * max_steer_angle, current_turn_angle - steer_speed))
-	if Input.is_action_pressed("Right"):
-		turn_angle = deg_to_rad(min(max_steer_angle, current_turn_angle + steer_speed))
+		acceleration = transform.x * engine_power
+	if Input.is_action_pressed("Brake"):
+		acceleration = transform.x * braking
 	
-	#Calculate Current Wheel Positions
-	# 𝑥2 = 𝑥1 + length * cos⁡(𝜃)
-	# y2 = y2 + length * sin(𝜃)
-	front_wheel.position.x = position.x + (wheel_base / 2) * cos(rotation)
-	front_wheel.position.y = position.y + (wheel_base / 2) * sin(rotation)
-	back_wheel.position.x = position.x + (wheel_base / 2) * cos(rotation + deg_to_rad(180))
-	back_wheel.position.y = position.y + (wheel_base / 2) * sin(rotation + deg_to_rad(180))
-	#Calculate Wheel Rotations
-	front_wheel.rotation = rotation + turn_angle
-	back_wheel.rotation = rotation
-	#Calculate New Wheel Positions
-	front_wheel.position.x = front_wheel.position.x + current_speed * cos(front_wheel.rotation)
-	front_wheel.position.y = front_wheel.position.y + current_speed * sin(front_wheel.rotation)
-	#Calculate New Car Rotation
-	rotation = (front_wheel.rotation + back_wheel.rotation) / 2
-	#Calculate New Car Position
-	position.x = (front_wheel.position.x + back_wheel.position.x) / 2
-	position.y = (front_wheel.position.y + back_wheel.position.y) / 2
+	#Apply Friction
+	if acceleration == Vector2.ZERO and velocity.length() < 50:
+		velocity = Vector2.ZERO	
+	var friction_force = velocity * friction * delta
+	var drag_force = velocity * velocity.length() * drag * delta
+	acceleration += drag_force + friction_force
 	
+	#Calculate steering
+	# 1. Find the wheel positions
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	# 2. Move the wheels forward
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	# 3. Find the new direction vector
+	var new_heading = rear_wheel.direction_to(front_wheel)
+	#Traction
+	var traction = traction_slow
+	if velocity.length() > slip_speed:
+		traction = traction_fast
+	# 4. Set the velocity and rotation to the new direction
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0: #Forwards
+		velocity = lerp(velocity, new_heading * velocity.length(), traction * delta)
+	elif d < 0: #Backwards
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+	rotation = new_heading.angle()
+	
+	#Calculate Velocity & move_and_slide()
+	velocity += acceleration * delta
 	move_and_slide()
